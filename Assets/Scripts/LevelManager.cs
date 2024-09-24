5e147f5c5beb3eaf4febaa968c4202714b0de8ed
Yavuz Selim Yıldız <yavuzslmyldz@gmail.com>
2024-09-25 00:22:50 +0300

    assssssset güncellemesi

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject floorPrefab;
+    
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
