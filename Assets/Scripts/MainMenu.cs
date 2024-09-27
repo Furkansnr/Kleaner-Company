9a6098312339ab737c78a8f6859b009df443cae7
Yavuz Selim Yıldız <yavuzslmyldz@gmail.com>
2024-09-28 02:00:23 +0300

    upgrade mainmenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening.Core.Easing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,16 +12,52 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button QuitB;
     [SerializeField] private Button SettingsB;
     [SerializeField] private Button SoundB;
-    void Start()
+    [SerializeField] private Button[] BackB;
+
+    [SerializeField] private GameObject CreditsP;
+    [SerializeField] private GameObject SettingsP;
+    void OnEnable()
     {
         StartB.onClick.AddListener(StartButton);
+        CreditsB.onClick.AddListener(CreditsButton);
         QuitB.onClick.AddListener(QuitButton);
+        SettingsB.onClick.AddListener(SettingsButton);
+        SoundB.onClick.AddListener(SoundButton);
     }
+    void OnDisable()
+    {
+        StartB.onClick.RemoveAllListeners();
+        CreditsB.onClick.RemoveAllListeners();
+        QuitB.onClick.RemoveAllListeners();
+        SettingsB.onClick.RemoveAllListeners();
+        SoundB.onClick.RemoveAllListeners();
+    }
+    
 
 
     void StartButton()
     {
         SceneManager.LoadScene(1);
+    }
+    void CreditsButton()
+    {
+        CreditsP.SetActive(true);
+        BackB[0].onClick.AddListener(() => BackButton(CreditsP,0));
+    }
+    void SettingsButton()
+    {
+        SettingsP.SetActive(true);
+        BackB[1].onClick.AddListener(() => BackButton(SettingsP,1));
+    }
+    void SoundButton()
+    {
+        
+    }
+
+    void BackButton(GameObject closedPanel, int i)
+    {
+        closedPanel.SetActive(false);
+        BackB[i].onClick.RemoveAllListeners();
     }
 
     void QuitButton()
